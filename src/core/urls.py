69208cb973def2a16a3074bd5f7f88076304a5d7
Evgeniy Goncharuck <iterlace.dev@sent.com>
2022-05-30 00:24:40 +0300

    swagger

--- src/core/urls.py
+++ src/core/urls.py
@@ -3,10 +3,38 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from movies.urls import router as movies_router
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Movies API",
+        default_version="v1",
+        description="",
+        contact=openapi.Contact(email="iterlace@knu.ua"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
     path("", include(movies_router.urls)),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
